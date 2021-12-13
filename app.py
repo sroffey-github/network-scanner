6704a49a62f2a13a315ba425ea3d6e51c1c4ab35
sroffey-github <82031457+sroffey-github@users.noreply.github.com>
2021-12-13 13:46:41 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from os import closerange
 import scapy.all as scapy
-import time, datetime
+import time
+import subprocess
+import datetime
 
 NETWORK = "192.168.1.0/24"
-INTERVAL = 30  # seconds
-
+INTERVAL = 15  # seconds
 dictionary = {
-    "mac":"name"
+    '52:52:96:d9:12:a4':'Samsung S20 5G',
+    '98:8b:0a:b5:c3:85':'CCTV',
+    '58:2f:40:79:5e:ec':'Nintendo Switch',
+    '58:82:a8:13:28:67':'Xbox One',
+    '5c:96:56:2e:c5:cd':'Spencer PS4',
+    'd0:d7:83:7f:b8:14':'Huawei P20',
+    'd0:d7:83:cb:a1:b6':'Huawei P20',
+    'f4:5c:89:ad:ba:fb':'MacBook Pro - Work',
+    '02:84:48:33:a4:2a':'Galaxy Tab A',
+    'dc:a2:66:48:33:a4:2a':'Laptop Windows',
+    'e8:d8:19:2e:ca:93':'Luke PS4',
+    'b8:27:eb:c7:81:0c':'Raspberry Pi Zero',
 }
 
-def log(name, status):
+def log(device, status):
     with open('logs/log.txt', 'a') as f:
-        f.write(f'[{datetime.datetime.now()}] {status.upper()}: {name.title()}\n')
+        f.write(f'[{datetime.datetime.now()}] {status} - {device}\n')
+
 
 def scan(ip):
     macs = set()
@@ -24,18 +36,16 @@ def scan(ip):
             macs.add(host[1].src)
     return macs
 
+
 def connection_change(hosts, action):
     if action not in ("connected", "disconnected"):
         raise ValueError(f"Invalid action: {action}")
     for host in hosts:
-        if host.upper() in dictionary:
-            device = dictionary[host.upper()]
-            if action == 'connected':
-                log(device, 'connected')
-            else:
-                log(device, 'disconnected')
+        device = dictionary[host] if host in dictionary else 'unknown device'
+        if action == 'connected':
+            log(device, 'CONNECTED')
         else:
-            pass
+            log(device, 'DISCONNECTED')
 
 def main():
     old_macs = scan(NETWORK)
